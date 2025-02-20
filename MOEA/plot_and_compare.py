730fd3e97a8f327a41493090fcc7f8e3bb67b43c
mattiazaccarini <zccmtt@unife.it>
2025-02-20 10:56:57 +0100

    Create ILP objective files from ILP results and compute sparsity and hv for every use case gathering their results in specific folders.

--- MOEA/plot_and_compare.py
+++ MOEA/plot_and_compare.py
@@ -29,7 +29,40 @@ from scipy.spatial import ConvexHull
 from matplotlib import pyplot as plt
 import glob
 import os
+import csv
+import ast
 
+'''
+Retrieve the objectives from the ILP csv file
+'''
+def retrieve_objectives(filename, output_dir=None):
+    if output_dir is None:
+        output_dir = os.path.dirname(filename)
+    else:
+        os.makedirs(output_dir, exist_ok=True)
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # Retrieve the gap
+            gap = row["MIP_Gap"]
+            
+            # Convert the string representation of the lists to actual lists
+            try:
+                latency = ast.literal_eval(row["Latency"])
+                cost = ast.literal_eval(row["Cost"])
+                unavailability = ast.literal_eval(row["Unavailability"])
+            except Exception as e:
+                print(f"Parsing error for gap {gap}: {e}")
+                continue
+            
+            # Create the output file
+            output_filename = os.path.join(output_dir, f"objectives_gap_{gap}.txt")
+            
+            # Put the objectives in the file
+            with open(output_filename, "w") as outfile:
+                for lat, c, unav in zip(latency, cost, unavailability):
+                    outfile.write(f"{lat} {c} {unav}\n")
 
 '''
 Plot the Pareto front starting from the list of solutions
@@ -99,29 +132,51 @@ def sparsity_calculation(front, num_objective):
 
 def main():
     # Glob for each direcory within results
-    dir_usecases = glob.glob('results/*/')
+    dir_usecases_meta = glob.glob('results/*/')
+    dir_usecases_ilp = glob.glob('../results/usecase/*/')
+
+    # Retrieve the objectives from the ILP csv files
+    for usecase in dir_usecases_ilp:
+        output_dir = os.path.join(usecase, 'objectives')
+        os.makedirs(output_dir, exist_ok=True)
+        objectives_files_ilp = glob.glob(f'{usecase}/results*.csv')
+        for objective_file in objectives_files_ilp:
+            retrieve_objectives(objective_file, output_dir)
+
+
     ##### Metaheuristics metrics computation #####
     
     #src_str = 'MultiClusterGPU'
     #files = find_files(src_str)
 
 
-    for usecase in dir_usecases:
-        output_dir = os.path.dirname(usecase)
-        output_file_sparsity = os.path.join(output_dir, "sparsity.txt")
+    for usecase in dir_usecases_meta:
+        output_dir_sparsities = os.path.dirname('../sparsities/')
+        usecase_name = usecase.split('/')[1]
+        os.makedirs(output_dir_sparsities, exist_ok=True)
+        output_dir_hv = os.path.dirname('../hypervolumes/')
+        os.makedirs(output_dir_hv, exist_ok=True)
+        output_file_sparsity = os.path.join(output_dir_sparsities, f"sparsity_{usecase_name}.txt")
+        output_file_hv = os.path.join(output_dir_hv, f"hypervolume_{usecase_name}.txt")
         f_sparsity = open(output_file_sparsity, "w")
         usecase = usecase.split('/')[1]
         print(f'Usecase: {usecase}')
         objectives_files_meta = glob.glob(f'results/{usecase}/*.FUN.*')
+        objectives_files_ilp = glob.glob(f'../results/usecase/{usecase}/objectives/*.txt')
+    
+        objsilpgap00 = []
+        objsilpgap005 = []
+        objsilpgap01 = []
+        objsilpgap025 = []
         objsmocell = []
         objsnsgaii = []
         objsnsgaiii = []
         objsmspso = []
+
         for filename in objectives_files_meta:
             #if '.png' in filename:
             #    continue
             print(filename) 
-            output_file_hv = os.path.join(output_dir, "hypervolume.txt")
             
             solutions = read_solutions(filename)
             sparsity = sparsity_calculation(solutions, 3)
@@ -152,8 +207,25 @@ def main():
             #ylabel = src_str[2:4].lower()
             plot_3d_front(objective_values, algname, output_dir)#, xlabel, ylabel)
 
+        # Add sparsity calculation related to ILP for each gap
+        f_sparsity.write(f"\n\n********** ILP **********\n\n")
+        for filename in objectives_files_ilp:
+            gap = filename.split('_')[-1].split('.t')[0]
+            solutions = read_solutions(filename)
+            sparsity = sparsity_calculation(solutions, 3)
+            f_sparsity.write(f"ILP Gap {gap} Sparsity: {sparsity}\n")
+
+            if gap == '0.00':
+                objsilpgap00 = [s.objectives for s in solutions]
+            elif gap == '0.05':
+                objsilpgap005 = [s.objectives for s in solutions]
+            elif gap == '0.1':
+                objsilpgap01 = [s.objectives for s in solutions]
+            elif gap == '0.25':
+                objsilpgap025 = [s.objectives for s in solutions]
+
         # Put all objs into a numpy array
-        objs = np.array(objsmocell + objsnsgaii + objsnsgaiii + objsmspso)
+        objs = np.array(objsilpgap00 + objsilpgap005 + objsilpgap01 + objsilpgap025 + objsmocell + objsnsgaii + objsnsgaiii + objsmspso)
         
         reference_point = objs.max(axis=0) * 1.1
         reference_point = reference_point.tolist()
@@ -163,6 +235,10 @@ def main():
         hv_nsgaii = hv.compute(np.array(objsnsgaii))
         hv_nsgaiii = hv.compute(np.array(objsnsgaiii))
         hv_mspso = hv.compute(np.array(objsmspso))
+        hv_moilp_gap00 = hv.compute(np.array(objsilpgap00))
+        hv_moilp_gap005 = hv.compute(np.array(objsilpgap005))
+        hv_moilp_gap01 = hv.compute(np.array(objsilpgap01))
+        hv_moilp_gap025 = hv.compute(np.array(objsilpgap025))
 
         with open(output_file_hv, "w") as f:
             #f.write(f"Reference Point: {reference_point}\n")
@@ -170,28 +246,12 @@ def main():
             f.write(f"NSGAII Hypervolume: {hv_nsgaii}\n")
             f.write(f"NSGAIII Hypervolume: {hv_nsgaiii}\n")
             f.write(f"MSPSO Hypervolume: {hv_mspso}\n")
+            f.write(f"\n\n********** ILP **********\n\n")
+            f.write(f"ILP Gap 0.00 Hypervolume: {hv_moilp_gap00}\n")
+            f.write(f"ILP Gap 0.05 Hypervolume: {hv_moilp_gap005}\n")
+            f.write(f"ILP Gap 0.1 Hypervolume: {hv_moilp_gap01}\n")
+            f.write(f"ILP Gap 0.25 Hypervolume: {hv_moilp_gap025}\n")
 
-    ##### MO-ILP Metrics Computation #####
-    objectives_files_ilp = glob.glob('../results/usecase/*/objectives*.csv')
-
-    for objective_file in objectives_files_ilp:
-        output_dir = os.path.dirname(objective_file)
-        output_file_sparsity = os.path.join(output_dir, "sparsity.txt")
-        output_file_hv = os.path.join(output_dir, "hypervolume.txt")
-        solutions = read_solutions(objective_file)
-        sparsity = sparsity_calculation(solutions, 3)
-        with open(output_file_sparsity, "w") as f:
-                f.write(f"Sparsity: {sparsity}\n")
-        #print(f'Number of solutions: {len(solutions)}')
-        # Getting the objective values
-        objective_values = [s.objectives for s in solutions]
-        objs = np.array(objective_values)
-        reference_point = objs.max(axis=0) * 1.1
-        reference_point = reference_point.tolist()
-        hv = HyperVolume(reference_point)
-        hv_moilp = hv.compute(objs)
-        with open(output_file_hv, "w") as f:
-            f.write(f"Hypervolume: {hv_moilp}\n")
 
 if __name__ == '__main__':
     main()
